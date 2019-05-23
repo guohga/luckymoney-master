c7ea85b89f200e738be8db64a60f1a811db23e28
guo.kk@163.com <guo.kk@163.com>
2019-05-23 18:49:51 +0800

    test1123

---
+++ HelloController.java
@@ -0,0 +1,24 @@
+package com.imooc.luckymoney;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by guohga
+ * test11111
+ * 2019-02-27 17:54
+ * @Controller + @ResponseBody = @RestController
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@Autowired
+	private LimitConfig limitConfig;
+
+	@PostMapping("/say")
+	public String say(@RequestParam(value = "id", required = false, defaultValue = "0") Integer myId) {
+//		return "说明：" + limitConfig.getDescription();
+		return "id:" + myId;
+	}
+}
